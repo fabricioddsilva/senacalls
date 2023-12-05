0df9a6253447ac0e7c86cb5ea31fa44f2a62c795
lhrbf <l.henriquerbf07@gmail.com>
2023-12-05 19:32:43 -0300

    flatList

--- Front-end/components/todosChamados.js
+++ Front-end/components/todosChamados.js
@@ -1,88 +1,92 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 
 function TodosChamados() {
-    const [chamados, setChamados] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+  const [chamados, setChamados] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        axios.get("https://sua-api.com/chamados")
-            .then(response => {
-                setChamados(response.data); // Supondo que a API retorna uma lista de chamados
-            })
-            .catch(error => {
-                console.error("Erro ao buscar dados:", error);
-            })
-            .finally(() => {
-                setIsLoading(false);
-            });
-    }, []);
+  useEffect(() => {
+    axios.get("https://sua-api.com/call")
+      .then(response => {
+        setChamados(response.data);
+      })
+      .catch(error => {
+        console.error("Erro ao buscar dados:", error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
 
-    if (isLoading) {
-        return (
-            <View style={styles.loadingContainer}>
-                <Text>Loading...</Text>
-            </View>
-        );
-    }
+  const renderItem = ({ item }) => (
+    <View style={styles.divChamado}>
+      <Text style={styles.texto}>Nº Chamado: {item.num}</Text>
+      <Text>{item.descricao}</Text>
+      {item.concluido ? (
+        <View style={styles.concluido}>
+          <Text style={styles.concluidoTexto}>Concluído</Text>
+        </View>
+      ) : (
+        <View style={styles.emAndamento}>
+          <Text style={styles.emAndamentoTexto}>Em andamento</Text>
+        </View>
+      )}
+    </View>
+  );
 
+  if (isLoading) {
     return (
-        <ScrollView>
-            {chamados.map((chamado, index) => (
-                <View key={index} style={styles.divChamado}>
-                    <Text style={styles.texto}>Nº Chamado: {chamado.num}</Text>
-                    <Text>{chamado.descricao}</Text>
-                    {chamado.concluido ? (
-                        <View style={styles.concluido}>
-                            <Text style={styles.concluidoTexto}>Concluído</Text>
-                        </View>
-                    ) : (
-                        <View style={styles.emAndamento}>
-                            <Text style={styles.emAndamentoTexto}>Em andamento</Text>
-                        </View>
-                    )}
-                </View>
-            ))}
-        </ScrollView>
+      <View style={styles.loadingContainer}>
+        <Text>Loading...</Text>
+      </View>
     );
+  }
+
+  return (
+    <FlatList
+      data={chamados}
+      keyExtractor={(item, index) => index.toString()}
+      renderItem={renderItem}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
-    divChamado: {
-        backgroundColor: "orange",
-        marginBottom: 10,
-        padding: 10,
-        position: 'relative',
-    },
-    texto: {
-        fontWeight: 'bold',
-    },
-    loadingContainer: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    concluido: {
-        position: "absolute",
-        top: 5,
-        right: 5,
-        padding: 5,
-        backgroundColor: "green",
-    },
-    concluidoTexto: {
-        fontSize: 10,
-    },
-    emAndamento: {
-        position: "absolute",
-        top: 5,
-        right: 5,
-        padding: 5,
-        backgroundColor: "yellow",
-    },
-    emAndamentoTexto: {
-        fontSize: 10,
-    },
+  divChamado: {
+    backgroundColor: "orange",
+    marginBottom: 10,
+    padding: 10,
+    position: 'relative',
+  },
+  texto: {
+    fontWeight: 'bold',
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  concluido: {
+    position: "absolute",
+    top: 5,
+    right: 5,
+    padding: 5,
+    backgroundColor: "green",
+  },
+  concluidoTexto: {
+    fontSize: 10,
+  },
+  emAndamento: {
+    position: "absolute",
+    top: 5,
+    right: 5,
+    padding: 5,
+    backgroundColor: "yellow",
+  },
+  emAndamentoTexto: {
+    fontSize: 10,
+  },
 });
 
 export default TodosChamados;
--- Front-end/pages/Home.js
+++ Front-end/pages/Home.js
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import LogoSupEsq from "../components/logoSupEsq";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import BotoesFooter from "../components/botoesFooter";
 import TodosChamados from "../components/todosChamados";
 
@@ -14,39 +14,28 @@ function Home() {
       .catch((error) => console.error("Erro ao buscar dados:", error));
   }, []); 
 
+  const renderItem = ({ item }) => (
+    <TodosChamados
+      num={item.num}
+      descricao={item.descricao}
+      tipo={item.tipo}
+    />
+  );
+
   return (
     <View style={styles.container}>
       <LogoSupEsq />
-      <ScrollView contentContainerStyle={styles.scrollContainer}>
-        <View style={styles.chamadosContainer}>
-          {chamados
-            .filter((chamado) => chamado.tipo === "concluido")
-            .map((chamado, index) => (
-              <TodosChamados
-                key={index}
-                num={chamado.num}
-                descricao={chamado.descricao}
-                tipo={chamado.tipo}
-              />
-            ))}
-          {chamados
-            .filter((chamado) => chamado.tipo === "emAndamento")
-            .map((chamado, index) => (
-              <TodosChamados
-                key={index}
-                num={chamado.num}
-                descricao={chamado.descricao}
-                tipo={chamado.tipo}
-              />
-            ))}
-          
-          {chamados.length === 0 && (
-            <View style={styles.textContainer}>
-              <Text style={styles.textoErro}>Não há chamados disponíveis</Text>
-            </View>
-          )}
-        </View>
-      </ScrollView>
+      <FlatList
+        data={chamados}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={renderItem}
+        ListEmptyComponent={() => (
+          <View style={styles.textContainer}>
+            <Text style={styles.textoErro}>Não há chamados disponíveis</Text>
+          </View>
+        )}
+        contentContainerStyle={styles.chamadosContainer}
+      />
       <BotoesFooter />
     </View>
   );
@@ -55,10 +44,6 @@ function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  scrollContainer: {
-    flexGrow: 1,
-    backgroundColor: "white",
   },
   textContainer: {
     marginTop: 100,
@@ -70,6 +55,6 @@ const styles = StyleSheet.create({
   chamadosContainer: {
     padding: 5,
   },
-  });
+});
 
 export default Home;
